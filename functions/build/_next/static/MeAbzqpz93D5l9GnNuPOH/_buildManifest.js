23946ca5ac3785296308d1c491bccaac75363b45
Chenna.Sreenu <csrinu236@gmail.com>
2024-11-20 15:32:59 +0530

    working

--- functions/build/_next/static/MeAbzqpz93D5l9GnNuPOH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/8a9fd3bd5059caf6.css","static/chunks/pages/index-99b0bd472039cf97.js"],"/404":["static/chunks/pages/404-9f2ee14adbdbdd3b.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
